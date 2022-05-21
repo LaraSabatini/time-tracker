43d70251081b63be20c24ceb75b76d3ed2abfa0c
Lara <sabatinilara@gmail.com>
2022-05-21 11:53:37 -0300

    created theme and added font family

---
+++ theme/index.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    background: "#E5E5E5",
+    black: "#000000",
+    secondary: "#F7F7F7",
+    inputs: "#9E9E9E",
+    primary: "#3563E9",
+  },
+  fonts: {
+    primary: "Inter",
+  },
+  fontSizes: {
+    m: "16px",
+    s: "13px",
+  },
+  fontWeights: {
+    normal: "400",
+    medium: "500",
+    bold: "600",
+  },
+}
